e0c816b7d37109e760a6e79c4eb18e8103debacc
Akshay Madrikar <akshaymadrikar@gmail.com>
2020-04-09 15:49:34 +0530

    added header component with navigations

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 
 import HomePage from './singlePages/homepage/homepage.component';
-//import Header from './components/header/header.component';
+import Header from './components/header/header.component';
 import ContactPage from './singlePages/contact/contact.component';
 import ShopPage from './singlePages/shop/shop.component';
  
@@ -11,10 +11,11 @@ import './App.css';
 function App() {
   return (
     <div className="App">
+      <Header />
         <Switch>
           <Route exact path='/' component={HomePage} />
           <Route path='/contact' component={ContactPage} />
-          <Route exact path='/shop' component={ShopPage} />
+          <Route exact path='/explore' component={ShopPage} />
         </Switch>
     </div>
   );
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -1,9 +1,36 @@
 import React from 'react';
 
+import { Link } from 'react-router-dom';
+
+import { ReactComponent as Logo } from '../../assets/logo.svg';
+
 const Header = () => {
 
     return (
-        <h3>Mercholic</h3>
+    <div classNameName="container">
+        <nav className="navbar navbar-expand-lg navbar-light m-3">
+                <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo03" aria-controls="navbarTogglerDemo03" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+                </button>
+                <Link className="navbar-brand" to="/">Mercholic</Link>
+            
+                <div className="collapse navbar-collapse" id="navbarTogglerDemo03">
+                <ul className="navbar-nav ml-auto mt-2 mt-md-0">
+                    <li className="nav-item">
+                    <Link className="nav-link" to="/explore">Explore</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link className="nav-link" to="/contact">Contact</Link>
+                    </li>
+                    <li className="nav-item">
+                    <Link className="nav-link" to="/sign-in">Sign In</Link>
+                    </li>
+                </ul>
+                </div>
+        </nav>
+    </div>
+        
+       
     );
 };
 
