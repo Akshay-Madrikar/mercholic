f083dfac1ed0a7cf2076501be4700d998c8c943e
Akshay Madrikar <akshaymadrikar@gmail.com>
2020-04-12 18:56:25 +0530

    finished sign-in and sign-up components

--- src/singlePages/signInAndsignUp/signInAndsignUp.component.jsx
+++ src/singlePages/signInAndsignUp/signInAndsignUp.component.jsx
@@ -1,14 +1,22 @@
 import React from 'react';
 
 import SignIn from '../../components/sign-in/sign-in.component';
+import SignUp from '../../components/sign-up/sign-up.component';
 
 import './signInAndsignUp.styles.scss';
 
 
 const SignInAndSignUpPage = () => (
 
-   <div className="sign-in-sign-up">
-       <SignIn />
+   <div className="sign-in-sign-up conatiner">
+       <div className="row justify-content-between">
+            <div className="sign-in-col col-sm-12 col-md-6">
+                <SignIn />
+            </div>
+            <div className="sign-up-col col-sm-12 col-md-6">
+                <SignUp />
+            </div>
+       </div>
    </div>
 );
 
